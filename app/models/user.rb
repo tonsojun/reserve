2deb8ff7f08a076fa172bfc09e27decaf78ddc31
tonsojun <josephjun@gmail.com>
2023-04-17 14:45:52 -0700

    added customer and restaurant buttons for sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum user_type: {customer: 0, restaurant: 1}
+
+  def self.user_types
+    {"Customer" => 0, "Restaurant" => 1}
+  end
 end
